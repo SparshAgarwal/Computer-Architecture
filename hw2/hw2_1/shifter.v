022e36e91b65118366982038f361bc3aaa7865f1
Sparsh Agarwal <agarwal39@wisc.edu>
2017-02-12 19:29:18 -0600

    .

--- hw2/hw2_1/shifter.v
+++ hw2/hw2_1/shifter.v
@@ -4,31 +4,33 @@ module shifter (In, Cnt, Op, Out);
    input [3:0]  Cnt;
    input [1:0]  Op;
    output [15:0] Out;
+	wire [15:0] current_out;
 
-	(Cnt[3]&1'b1)?{
-		bit16_2to1mux(Op==2'd0,In[15:0],{In[7:0],In[15:8]},In[15:0]);
-		bit16_2to1mux(Op==2'd1,In[15:0],{In[7:0],8'b0},In[15:0]);
-		bit16_2to1mux(Op==2'd2,In[15:0],{{In[15]{8}},In[15:8]},In[15:0]);	
-		bit16_2to1mux(Op==2'd3,In[15:0],{8'b0,In[15:8]},In[15:0]);	
-		}:; 		
-	(Cnt[2]==1'b1)?{
-		bit16_2to1mux(Op==2'd0,In[15:0],{In[11:0],In[15,12]},In[15:0]);
-		bit16_2to1mux(Op==2'd1,In[15:0],{In[11:0],4'b0},In[15:0]);
-		bit16_2to1mux(Op==2'd2,In[15:0],{{In[15]{4}},In[15:4]},In[15:0]);	
-		bit16_2to1mux(Op==2'd3,In[15:0],{4'b0,In[15:4]},In[15:0]);
-		}:; 		
-	(Cnt[1]==1'b1)?{
-		bit16_2to1mux(Op==2'd0,In[15:0],{In[13:0],In[15,14]},In[15:0]);
-		bit16_2to1mux(Op==2'd1,In[15:0],{In[13:0],2'b0},In[15:0]);
-		bit16_2to1mux(Op==2'd2,In[15:0],{{In[15]{2}},In[15:2]},In[15:0]);	
-		bit16_2to1mux(Op==2'd3,In[15:0],{2'b0,In[15:2]},In[15:0]);
-		}:; 
-	(Cnt[0]==1'b1)?{
-		bit16_2to1mux(Op==2'd0,In[15:0],{In[14:0],In[15]},In[15:0]);
-		bit16_2to1mux(Op==2'd1,In[15:0],{In[14:0],1'b0},In[15:0]);
-		bit16_2to1mux(Op==2'd2,In[15:0],{{In[15]{1}},In[15:1]},In[15:0]);	
-		bit16_2to1mux(Op==2'd3,In[15:0],{1'b0,In[15:1]},In[15:0]);
-		}:; 
-	assign Out=In;
+	
+assign current_out[15:0]=Cnt[3]?((Op==2'd0)?{In[7:0],In[15:8]}:
+		((Op==2'd1)?{In[7:0],8'b0}:
+		((Op==2'd2)?{{8{In[15]}},In[15:8]}:
+		((Op==2'd3)?{8'b0,In[15:8]}:
+		In[15:0])))):
+		In[15:0];
+assign current_out[15:0]=Cnt[2]?((Op==2'd0)?{current_out[11:0],current_out[15:12]}:
+		((Op==2'd1)?{current_out[11:0],4'b0}:
+		((Op==2'd2)?{{4{current_out[15]}},current_out[15:4]}:
+		((Op==2'd3)?{4'b0,current_out[15:4]}:
+		current_out[15:0])))):
+		current_out[15:0];
+assign current_out[15:0]=Cnt[1]?((Op==2'd0)?{current_out[13:0],current_out[15:14]}:
+		((Op==2'd1)?{current_out[13:0],2'b0}:
+		((Op==2'd2)?{{2{current_out[15]}},current_out[15:2]}:
+		((Op==2'd3)?{2'b0,current_out[15:2]}:
+		current_out[15:0])))):
+		current_out[15:0];
+assign current_out[15:0]=Cnt[0]?((Op==2'd0)?{current_out[14:0],current_out[15]}:
+		((Op==2'd1)?{current_out[14:0],1'b0}:
+		((Op==2'd2)?{{1{current_out[15]}},current_out[15:1]}:
+		((Op==2'd3)?{1'b0,current_out[15:1]}:
+		current_out[15:0])))):
+		current_out[15:0];		 
+	assign Out=current_out;
 endmodule
 
