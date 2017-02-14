3d22c9dcce4b30d1003181e1e7c5b38156399b3a
Sparsh Agarwal <agarwal39@wisc.edu>
2017-02-13 20:26:35 -0600

    hw 2

--- hw2/hw2_2/carry_lookahead_16bit.v
+++ hw2/hw2_2/carry_lookahead_16bit.v
@@ -1,11 +1,11 @@
 module carry_lookahead_16bit(input [15:0] A,B,[0:0]Cin,
 			output Cout, [15:0] S);
 
-	wire cout;
+	wire cout[2:0];
 
-	carry_lookahead_4bit a1(A[3:0], B[3:0], Cin, cout, S[3:0]);
-	carry_lookahead_4bit a2(A[7:4], B[7:4], cout, cout, S[7:4]);
-	carry_lookahead_4bit a3(A[11:8], B[11:8], cout, cout, S[11:8]);
-	carry_lookahead_4bit a4(A[15:12], B[15:12], cout, Cout, S[15:12]);
+	carry_lookahead_4bit a1(A[3:0], B[3:0], Cin, cout[0], S[3:0]);
+	carry_lookahead_4bit a2(A[7:4], B[7:4], cout[0], cout[1], S[7:4]);
+	carry_lookahead_4bit a3(A[11:8], B[11:8], cout[1], cout[2], S[11:8]);
+	carry_lookahead_4bit a4(A[15:12], B[15:12], cout[2], Cout, S[15:12]);
 
 endmodule
--- hw2/hw2_2/carry_lookahead_4bit.v
+++ hw2/hw2_2/carry_lookahead_4bit.v
@@ -2,16 +2,16 @@ module carry_lookahead_4bit(input [3:0]A,B,[0:0]Cin,
 			output Cout, [3:0]S);
 
 	wire cout1,cout2,cout3;
-	wire p;
-	wire g;
+	wire p[3:0];
+	wire g[3:0];
 
-	fadd1bit a1(A[0], B[0], Cin, S[0], p, g);
-	assign cout1 = g|(Cin&p);
-	fadd1bit a2(A[1], B[1], cout, S[1], p, g);
-	assign cout2 = g|(cout1&p);
-	fadd1bit a3(A[2], B[2], cout, S[2], p, g);
-	assign cout3 = g|(cout2&p);
-	fadd1bit a4(A[3], B[3], cout, S[3], p, g);
-	assign Cout = g|(cout&p);
+	fadd1bit a1(A[0], B[0], Cin, S[0], p[0], g[0]);
+	assign cout1 = g[0]|(Cin&p[0]);
+	fadd1bit a2(A[1], B[1], cout, S[1], p[1], g[1]);
+	assign cout2 = g[1]|(cout1&p[1]);
+	fadd1bit a3(A[2], B[2], cout, S[2], p[2], g[2]);
+	assign cout3 = g[2]|(cout2&p[2]);
+	fadd1bit a4(A[3], B[3], cout, S[3], p[3], g[3]);
+	assign Cout = g[3]|(cout&p[3]);
 
 endmodule
