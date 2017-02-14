b27376ab9e7086d4be545a2e77b960e1f278b04c
Sparsh <agarwal39@wisc.edu>
2017-02-13 22:16:49 -0600

    hw2 added

--- hw2/hw2_2/alu.v
+++ hw2/hw2_2/alu.v
@@ -12,15 +12,14 @@ module alu (A, B, Cin, Op, invA, invB, sign, Out, Ofl, Z);
         output Z;
 
 	reg [15:0]value;
-	reg zero_value = 0'b0;
-	wire [15:0] sA,sB,w1,w2,w3,w4,w5,cout;
+	wire ofl, [15:0] sA,sB,w1,w2,w3,w4,w5,cout;
 
 	assign sA = invA? ~A:A;
 	assign sB = invB? ~B:B;
 	
-	carry_lookahead_16bit a1(sA,b,Cin,cout,w5)
-	assign Ofl = sign?((sA[15]~^sB[15])&cout? 1'b1:1'b0 ):1'b0;
-
+	assign ofl = 1'b0;
+	carry_lookahead_16bit a1(sA,sB,Cin,cout,w5);
+	
 	shifter s1(sA, sB[3:0], Op[1:0], w1);
 
 always @ (*) begin
@@ -30,6 +29,7 @@ always @ (*) begin
 		end
 		3'o4:begin
 			value = w5;
+			assign ofl = sign?((sA[15]~^sB[15])&cout? 1'b1:1'b0 ):1'b0;
 		end
 		3'o5:begin
 			value = sA|sB;
@@ -47,6 +47,6 @@ always @ (*) begin
 end
 
 assign Out=value;
-assign Z=zero_value;
+assign Z=&value;
     
 endmodule
