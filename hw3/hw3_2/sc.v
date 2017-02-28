9de698c5d05b52cf0966715b50dfc4f127316a75
Sparsh Agarwal <agarwal39@wisc.edu>
2017-02-27 18:36:06 -0600

    commit

--- hw3/hw3_2/sc.v
+++ hw3/hw3_2/sc.v
@@ -8,33 +8,42 @@ module sc( clk, rst, ctr_rst, out, err);
    input ctr_rst;
    output [2:0] out;
    output err;
-
-	reg[2:0] counter;
+   wire [2:0]                state;
+   reg [2:0]                 next_state;
 	
-	always@(*)begin
-		casex(counter)
-			3'b000:begin
-					counter = ctr_rst?3'b000:3'b001;
-				end
-			3'b001:begin
-					counter = ctr_rst?3'b000:3'b010;
-				end
-			3'b010:begin
-					counter = ctr_rst?3'b000:3'b011;
-				end
-			3'b011:begin
-					counter = ctr_rst?3'b000:3'b100;
-				end
-			3'b1??:begin
-					counter = ctr_rst?3'b000:3'b101;
-				end
-			default:begin
-					$display("error");
-				end
-		endcase
-		counter = rst?3'b000:counter;
-	end  
-assign out = counter;
+
+dff curr_state[2:0] (
+                        // Outputs
+                        .q              (state),
+                        // Inputs
+                        .d              (next_state),
+                        .clk            (clk),
+                        .rst            (rst));	
+
+always@(*)begin
+	casex(state)
+		3'b000:begin
+				next_state = rst?3'b000:(ctr_rst?3'b000:3'b001);
+			end
+		3'b001:begin
+				next_state  = rst?3'b000:(ctr_rst?3'b000:3'b010);
+			end
+		3'b010:begin
+				next_state  = rst?3'b000:(ctr_rst?3'b000:3'b011);
+			end
+		3'b011:begin
+				next_state  = rst?3'b000:(ctr_rst?3'b000:3'b100);
+			end
+		3'b1??:begin
+				next_state  = rst?3'b000:(ctr_rst?3'b000:3'b101);
+			end
+		default:begin
+				$display("error");
+			end
+	endcase
+	
+end  
+assign out = state;
 //hello
 endmodule
 
