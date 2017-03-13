f55a52d7993e0f417fc681b6e64f817c4ed55187
Sparsh Agarwal <agarwal39@wisc.edu>
2017-03-13 00:37:30 -0500

    project

--- project/project/demo1/verilog/proc_hier_bench.v
+++ project/project/demo1/verilog/proc_hier_bench.v
@@ -120,35 +120,36 @@ module proc_hier_bench();
    /* Assign internal signals to top level wires
       The internal module names and signal names will vary depending
       on your naming convention and your design */
+	  
 
    // Edit the example below. You must change the signal
    // names on the right hand side
     
-   assign PC = DUT.p0.fetch0.pcCurrent;
-   assign Inst = DUT.p0.fetch0.instr;
+   assign PC = DUT.p0.pcCurrent;
+   assign Inst = DUT.p0.instrOut; //instr
    
-   assign RegWrite = DUT.p0.decode0.regFile0.write;
+   assign RegWrite = DUT.p0.regWrEn; //write
    // Is register being written, one bit signal (1 means yes, 0 means no)
    
-   assign WriteRegister = DUT.p0.decode0.regFile0.writeregsel;
+   assign WriteRegister = DUT.p0.writeregsel; //writeregsel
    // The name of the register being written to. (3 bit signal)
 
-   assign WriteData = DUT.p0.decode0.regFile0.writedata;
+   assign WriteData = DUT.p0.writedata; //writedata
    // Data being written to the register. (16 bits)
    
-   assign MemRead =  DUT.p0.memory0.memRead;
+   assign MemRead =  DUT.p0.memRdEn; //memRead
    // Is memory being read, one bit signal (1 means yes, 0 means no)
    
-   assign MemWrite = (DUT.p0.memory0.memReadorWrite & DUT.p0.memory0.memWrite);
+   assign MemWrite = DUT.p0.memWrEn; //memWrite
    // Is memory being written to (1 bit signal)
    
-   assign MemAddress = DUT.p0.memory0.aluResult;
+   assign MemAddress = DUT.p0.mainALUresult; //aluResult
    // Address to access memory with (for both reads and writes to memory, 16 bits)
    
-   assign MemData = DUT.p0.memory0.writeData;
+   assign MemData = DUT.p0.read2data; //writeData
    // Data to be written to memory for memory writes (16 bits)
    
-   assign Halt = DUT.p0.memory0.halt;
+   assign Halt = DUT.p0.halt; //halt
    // Is processor halted (1 bit signal)
    
    /* Add anything else you want here */
