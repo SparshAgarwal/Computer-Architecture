a1aeaa94103c4347c080411357adaba41a7bd33a
Sparsh Agarwal <agarwal39@wisc.edu>
2017-04-11 20:10:08 -0500

    commit

--- hw5/hw5_1/p1.asm
+++ hw5/hw5_1/p1.asm
@@ -1,1 +1,14 @@
-// Write your assembly program for Problem 1 (a) here.
+lbi r1, 1
+lbi r3, 2
+SUB r2, r1, r3
+ADD r7, r2, r5
+XOR r2, r2, r6
+ADD r3, r2, r5
+ADD r7, r4, r2
+SUB r3, r4, r2
+ST r3, r2, 0
+ADD r7, r4, r2
+ST r4, r7, 1
+LD r5, r4, 2
+ST r5, r6, 1
+
--- hw5/hw5_2/p2.asm
+++ hw5/hw5_2/p2.asm
@@ -1,1 +1,6 @@
-// Write your assembly program for Problem 2 (a) here.
+lbi r1,0
+beqz r1,2
+lbi r1,2
+bnez r1,4
+lbi r1,2
+
