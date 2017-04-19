4b130ef0685d058cab70118d42c11f24f59c79cc
Sparsh Agarwal <agarwal39@wisc.edu>
2017-04-18 22:37:35 -0500

    commit

--- project/project/cache_direct/verilog/mem_system.v
+++ project/project/cache_direct/verilog/mem_system.v
@@ -23,30 +23,25 @@ module mem_system(/*AUTOARG*/
    output CacheHit;
    output err;
    
-   reg [4:0] nextState;
-   wire [4:0] currState;
-   dff curr_State[4:0] (.q(currState), .d(nextState), .clk(clk), .rst(rst));
+   reg [3:0] nextState;
+   wire [3:0] currState;
+   dff curr_State[3:0] (.q(currState), .d(nextState), .clk(clk), .rst(rst));
    
-   wire dirty, valid, hit, miss, mem_stall;
+   wire dirty, valid, hit, miss, mem_stall, cacheErr, memErr, cacheHit, cacheHitTemp;
+   reg comp, cache_write, mem_wr, mem_rd, enable, valid_in, controlEr, retry;
    wire[15:0] cache_data_out, mem_data_out;
    wire[4:0] tag_out;
    wire[3:0] busy;
-   reg comp, cache_write, mem_wr, mem_rd, enable, valid_in;
    reg [15:0] cache_data_in, mem_addr, mem_data_in;
    reg [7:0] index;
    reg [4:0] tag_in;
    reg [2:0] offset;
-   
-   wire cacheErr, memErr;
-   reg controlErr;
-   assign err = controlErr;
-   reg retry;
 
-   wire cHit1, cHit2;
-   //assign CacheHit = hit & comp & ~retry;
-   assign cHit1 = hit & comp & ~retry;
-   assign CacheHit = cHit2;
-   dff hitdff(.clk(clk), .rst(rst), .q(cHit2), .d(cHit1));
+   assign miss = enable  & (~hit | ~valid); 
+   assign err = controlErr;
+   assign cacheHit = hit & comp & ~retry;
+   assign CacheHit = cacheHitTemp;
+   dff hitdff(.clk(clk), .rst(rst), .q(cacheHitTemp), .d(cacheHit));
 
    /* data_mem = 1, inst_mem = 0 *
     * needed for cache parameter */
@@ -86,12 +81,33 @@ module mem_system(/*AUTOARG*/
                      .rd                	(mem_rd));
 
    
-   assign miss = enable  & (~hit | ~valid); 
+
    
    always@(*)begin case(currState)
-
-   		//RETRY
-   		5'h15: begin
+	
+		//IDLE
+		4'h0: begin 
+			retry = 0;
+			enable = 1;
+			comp = (~Wr & ~Rd) ? 0 : 1;
+			cache_write = Wr ? 1 : 0; 
+			index = (~Wr & ~Rd) ? 0 : Addr[10:3]; 
+			tag_in = (~Wr & ~Rd) ? 0 : Addr[15:11];
+			offset = (~Wr & ~Rd) ? 0 : {Addr[2:1], 1'b0};
+			cache_data_in = Wr ? DataIn : 0; 
+			valid_in = 0;
+			mem_addr = 0; 
+			mem_data_in = 0;
+			mem_wr = 0;
+			mem_rd = 0;
+			DataOut = Rd ? (( hit & valid ) ? cache_data_out : 0) : 0;
+			Done = (hit & valid & (Wr | Rd)) ? 1 : 0;
+			Stall = (~Wr & ~Rd) ? 0 :(( hit & valid) ? 0 : 1);
+			nextState = (~Wr & ~Rd) ? 0 : ((miss & ~dirty) ? 4'h2 : ((miss & valid & dirty) ? 4'hD :  4'h0));
+		end
+		
+		//RETRY
+   		4'h1: begin
    			retry = 1;
 			enable = 1;
 			comp = 1;
@@ -108,32 +124,11 @@ module mem_system(/*AUTOARG*/
 			DataOut = Rd ? cache_data_out  : 0;
 			Done = 0;
 			Stall = 1;
-			nextState = 5'h00;
+			nextState = 4'h0;
    		end
-	
-		//IDLE
-		5'h00: begin 
-			retry = 0;
-			enable = 1;
-			comp = (~Wr & ~Rd) ? 0 : 1;
-			cache_write = Wr ? 1 : 0; 
-			index = (~Wr & ~Rd) ? 0 : Addr[10:3]; 
-			tag_in = (~Wr & ~Rd) ? 0 : Addr[15:11];
-			offset = (~Wr & ~Rd) ? 0 : {Addr[2:1], 1'b0};
-			cache_data_in = Wr ? DataIn : 0; 
-			valid_in = 0;
-			mem_addr = 0; //not using memory yet
-			mem_data_in = 0;
-			mem_wr = 0;
-			mem_rd = 0;
-			DataOut = Rd ? (( hit & valid ) ? cache_data_out : 0) : 0;
-			Done = (hit & valid & (Wr | Rd)) ? 1 : 0;
-			Stall = (~Wr & ~Rd) ? 0 :(( hit & valid) ? 0 : 1);
-			nextState = (~Wr & ~Rd) ? 0 : ((miss & ~dirty) ? 5'h03 : ((miss & valid & dirty) ? 5'h0D : ((hit & valid) ? 5'h00 : 5'h00)));
-		end
 		
 		//MEMORY READ 1 
-		5'h03: begin 
+		4'h2: begin 
 			retry = 0;
 			enable = 0;
 			comp = 0;
@@ -144,17 +139,17 @@ module mem_system(/*AUTOARG*/
 			cache_data_in = 0; 
 			valid_in = 0;
 			mem_addr = {Addr[15:3], 3'b000};
-			mem_data_in = 0; //not writing to memory
+			mem_data_in = 0; 
 			mem_wr = 0;
 			mem_rd = 1;
-			DataOut = 0; //DataOut not set yet
+			DataOut = 0; 
 			Done = 0;
 			Stall = 1;
-			nextState = 5'h06; 
+			nextState = 4'h3; 
 		end
 		
 		//MEMORY READ 2
-		5'h06: begin 
+		4'h3: begin 
 			retry = 0;
 			enable = 0;
 			comp = 0;
@@ -168,14 +163,14 @@ module mem_system(/*AUTOARG*/
 			mem_data_in = 0; 
 			mem_wr = 0;
 			mem_rd = 1;
-			DataOut = 0; //DataOut not set yet
+			DataOut = 0; 
 			Done = 0;
 			Stall = 1;
-			nextState = mem_stall ? 5'h06 : 5'h08;
+			nextState = mem_stall ? 4'h3 : 4'h4;
 		end
 	
 		//MEMORY READ 3 and INSTALL CACHE 1
-		5'h08: begin 
+		4'h4: begin 
 			retry = 0;
 			enable = 1;
 			comp = 0;
@@ -183,7 +178,6 @@ module mem_system(/*AUTOARG*/
 			index = Addr[10:3];
 			tag_in = Addr[15:11];
 			offset = 3'b000;
-			//cache_data_in = (Wr & (offset == {Addr[2:1], 1'b0})) ? DataIn : mem_data_out; 
 			cache_data_in = mem_data_out;
 			valid_in = 0;
 			mem_addr = {Addr[15:3], 3'b100};
@@ -193,11 +187,11 @@ module mem_system(/*AUTOARG*/
 			DataOut = (Rd & (offset == {Addr[2:1], 1'b0})) ? mem_data_out : 0; 
 			Done = 0;
 			Stall = 1;
-			nextState = mem_stall ? 5'h08 : 5'h0A;
+			nextState = mem_stall ? 4'h4 : 4'h5;
 		end	
 		
 		//MEMORY READ 4 and INSTALL CACHE 2
-		5'h0A: begin 
+		4'h5: begin 
 			retry = 0;
 			enable = 1;
 			comp = 0;
@@ -205,7 +199,6 @@ module mem_system(/*AUTOARG*/
 			index = Addr[10:3];
 			tag_in = Addr[15:11];
 			offset = 3'b010;
-			//cache_data_in = (Wr & (offset == {Addr[2:1], 1'b0})) ? DataIn : mem_data_out; 
 			cache_data_in = mem_data_out;
 			valid_in = 0;
 			mem_addr = {Addr[15:3], 3'b110};
@@ -215,11 +208,11 @@ module mem_system(/*AUTOARG*/
 			DataOut = (Rd & (offset == {Addr[2:1], 1'b0})) ? mem_data_out : DataOut;
 			Done = 0;
 			Stall = 1;
-			nextState = mem_stall ? 5'h0A : 5'h0B;
+			nextState = mem_stall ? 4'h5 : 4'h6;
 		end
 
 		//INSTALL CACHE 3 
-		5'h0B: begin
+		4'h6: begin
 			retry = 0;
 			enable = 1;
 			comp = 0;
@@ -227,7 +220,6 @@ module mem_system(/*AUTOARG*/
 			index = Addr[10:3]; 
 			tag_in = Addr[15:11];
 			offset = 3'b100;
-			//cache_data_in = (Wr & (offset == {Addr[2:1], 1'b0})) ? DataIn : mem_data_out; 
 			cache_data_in = mem_data_out;
 			valid_in = 0;
 			mem_addr = 0; 
@@ -237,11 +229,11 @@ module mem_system(/*AUTOARG*/
 			DataOut = (Rd & (offset == {Addr[2:1], 1'b0})) ? mem_data_out : DataOut;
 			Done = 0;
 			Stall = 1;
-			nextState = mem_stall ? 5'h0B : 5'h0C; 
+			nextState = mem_stall ? 4'h6 : 4'h7; 
 		end 
 
 		//INSTALL CACHE 4 
-		5'h0C: begin
+		4'h7: begin
 			retry = 0;
 			enable = 1;
 			comp = 0;
@@ -249,116 +241,109 @@ module mem_system(/*AUTOARG*/
 			index = Addr[10:3]; 
 			tag_in = Addr[15:11];
 			offset = 3'b110;
-			cache_data_in = (~Rd & (offset == {Addr[2:1], 1'b0})) ? DataIn : mem_data_out; 
-			//cache_data_in = mem_data_out;
+			cache_data_in = mem_data_out;
 			valid_in = 1;
 			mem_addr = 0; 
 			mem_data_in = 0;
 			mem_wr = 0;
 			mem_rd = 0;
 			DataOut = (Rd & (offset == {Addr[2:1], 1'b0})) ? mem_data_out : DataOut;
-			//Done = 1;
 			Done = 0;
 			Stall = 1;
-			//nextState = 5'h00; 
-			nextState = 5'h15;
+			nextState = 4'h1; //retry and get a hit
 		end 
 		
 		
-		//PRE-MEMORY WRITEBACK/ACCESS READ 1 (miss, dirty, valid): read dirty data to writeback to memory in MEMORY WRITEBACK stage
-		5'h0D: begin
+		//PRE-MEMORY WRITEBACK/ACCESS READ 1 
+		4'h8: begin
 			retry = 0;
 			enable = 1;
 			comp = 0;
-			cache_write = 0; //not writing to cache
+			cache_write = 0; 
 			index = Addr[10:3]; 
-			tag_in = 0; //not used
+			tag_in = 0; 
 			offset = 3'b000;
-			cache_data_in = 0; //not using
+			cache_data_in = 0; 
 			valid_in = 0;
 			mem_addr = {tag_out, Addr[10:3], 3'b000};
 			mem_data_in = cache_data_out;
 			mem_wr = 1;
 			mem_rd = 0;
-			DataOut = 0; //DataOut not set yet
+			DataOut = 0; 
 			Done = 0;
 			Stall = 1;
-			nextState = 5'h0E;
+			nextState = 4'h9;
 		end
 		
 		//PRE-MEMORY WRITEBACK/ACCESS READ 2 (and WRITE 1)
-		5'h0E: begin
+		4'h9: begin
 			retry = 0;
 			enable = 1;
 			comp = 0;
-			cache_write = 0; //not writing to cache
+			cache_write = 0; 
 			index = Addr[10:3]; 
-			tag_in = 0; //not used
+			tag_in = 0; 
 			offset = 3'b010;
-			cache_data_in = 0; //not using
+			cache_data_in = 0; 
 			valid_in = 0;
 			mem_addr = {tag_out, Addr[10:3], 3'b010};
 			mem_data_in = cache_data_out;
 			mem_wr = 1;
 			mem_rd = 0;
-			DataOut = 0; //DataOut not set yet
+			DataOut = 0; 
 			Done = 0;
 			Stall = 1;
-			nextState = mem_stall ? 5'h0E : 5'h0F;
+			nextState = mem_stall ? 4'h9 : 4'hA;
 		end
 
 		//PRE-MEMORY WRITEBACK/ACCESS READ 3 (and WRITE 1 and 2)
-		5'h0F: begin
+		4'hA: begin
 			retry = 0;
 			enable = 1;
 			comp = 0;
-			cache_write = 0; //not writing to cache
+			cache_write = 0; 
 			index = Addr[10:3]; 
-			tag_in = 0; //not used
+			tag_in = 0; 
 			offset = 3'b100;
-			cache_data_in = 0; //not using
+			cache_data_in = 0; 
 			valid_in = 0;
 			mem_addr = {tag_out, Addr[10:3], 3'b100};
 			mem_data_in = cache_data_out;
 			mem_wr = 1;
 			mem_rd = 0;
-			DataOut = 0; //DataOut not set yet
+			DataOut = 0; 
 			Done = 0;
 			Stall = 1;
-			nextState = mem_stall ? 5'h0F : 5'h10;
+			nextState = mem_stall ? 4'hA : 4'hB;
 		end	
 
 		//PRE-MEMORY WRITEBACK/ACCESS READ 4 (and WRITE 1, 2, 3)
-		5'h10: begin
+		4'hB: begin
 			retry = 0;
 			enable = 1;
 			comp = 0;
-			cache_write = 0; //not writing to cache
+			cache_write = 0; 
 			index = Addr[10:3]; 
-			tag_in = 0; //not used
+			tag_in = 0; 
 			offset = 3'b110;
-			cache_data_in = 0; //not using
+			cache_data_in = 0; 
 			valid_in = 0;
 			mem_addr = {tag_out, Addr[10:3], 3'b110};
 			mem_data_in = cache_data_out;
 			mem_wr = 1;
 			mem_rd = 0;
-			DataOut = 0; //DataOut not set yet
+			DataOut = 0; 
 			Done = 0;
 			Stall = 1;
-			nextState = mem_stall ? 5'h10 : 5'h03;
+			nextState = mem_stall ? 4'hB : 4'h2;
 		end
 		
 		//ERROR
-		5'h15: begin
+		4'hC: begin
 			Done = 1;
 			Stall = 0;
-			if(Done && 1) begin
-				$display(" done in error");
-				end
 			controlErr = 1;
-			nextState = 5'h00;
-			$display("next state: %d",nextState);
+			nextState = 4'h0;
 		end
 		
 		default: begin
@@ -366,19 +351,19 @@ module mem_system(/*AUTOARG*/
 			enable = 1;
 			comp = 1;
 			cache_write = Wr ? 1 : 0; 
-			index = (~Wr & ~Rd) ? 0 : Addr[10:3]; //[1:0] is to select word in a cache line
+			index = (~Wr & ~Rd) ? 0 : Addr[10:3]; 
 			tag_in = (~Wr & ~Rd) ? 0 : Addr[15:11];
 			offset = (~Wr & ~Rd) ? 0 : {Addr[2:1], 1'b0};
 			cache_data_in = Wr ? DataIn : 0; 
 			valid_in = 0;
-			mem_addr = 0; //not using memory yet
+			mem_addr = 0;
 			mem_data_in = 0;
 			mem_wr = 0;
 			mem_rd = 0;
 			DataOut = Rd ? (( hit & valid ) ? cache_data_out : 0) : 0;
 			Done = (hit & valid) ? 1 : 0;
 			Stall = (~Wr & ~Rd) ? 0 :(( hit & valid) ? 0 : 1);
-			nextState = (~Wr & ~Rd) ? 0 : ((miss & ~dirty) ? 5'h03 : ((miss & valid & dirty) ? 5'h0D : ((hit & valid) ? 5'h00 : 5'h00)));
+			nextState = (~Wr & ~Rd) ? 0 : ((miss & ~dirty) ? 4'h2 : ((miss & valid & dirty) ? 4'h8 : ((hit & valid) ? 4'h0 : 4'h0)));
 		end
 	
 	endcase
